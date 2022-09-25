b2fff93ab7dfb35d4aad202eab638413c5a2c396
haseeb-farahi <111962155+haseeb-farahi@users.noreply.github.com>
2022-09-25 11:57:45 +0430

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,78 @@
+// $('div').click(()=>{
+//     $('div').animate({left: '300px' , height :'200px' , width : '500px' , opacity : 'o.5'},3000,()=>{
+//         alert('im just called')
+// })
+// $('div').click(()=>{}
+// var div = $('div');
+// div.animate({height: '200px',},'slow');
+// div.animate({width: '300px',},'slow');
+// div.animate({width: '100px', left:"300px"},'slow')
+// div.animate({width: '100px', },'slow')
+// })
+
+
+
+// $('button').click(()=>{
+//     $('div').stop();
+// });
+
+// $('h1').click(()=>{
+//     $('p').slideToggle(3000);
+
+// });
+// $('button').click(()=>{
+//     $('p').stop();
+// })
+// $('p').click(()=>{
+//     $('p').hide(2000,()=>{
+//         alert('hide it')
+//     })
+    
+// })
+
+
+
+// $('h1').click(()=>{
+//     $('p').css('color', 'blue').slideUp(2000).slideDown(3000).animate(
+//         {
+//             width:'200px'
+//         },3000
+//     )
+// })
+
+// $('h1').click(()=>{
+    // $('h1').text('the text message'+$('p').text())
+    // alert('this is the text() Method'+ $('p').text());
+    // alert('this is the text() Method'+ $('input').val());
+    // alert('this is the text() Method'+ $('p').html());
+  
+
+// })
+$('h2').click(()=>{
+    // document.querySelector('h5').innerHTML = '<h5>this is message is passed from js file</h5></mark>'
+    // $('input').val('This is message is passed from js file')
+    // document.querySelector('h2').innerHTML = 'this is message is passed from js file'
+    // alert('the atribute:' +$('p').attr('style'))
+   
+//     console.log($('a').attr({
+//         'href':'https://yahoo.com',
+//         'title':'hi',
+//         'style':'color:red;'
+//     }))
+
+// $('h2').click(()=>{
+//     // $('h2').append($('p').text())
+//     $('h2').after($('p').text())
+
+// })
+
+var theP = $('p');
+var theStrong = $('strong');
+var thesmall = $('small');
+var themark = $('mark');
+
+
+$('ol').append('<li>something</lo>')
+})
+
+
